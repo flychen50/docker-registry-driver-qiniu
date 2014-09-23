804f6b1b993db67909e51e7f828d37d16fc71fb7
zhangpeihao <zhangpeihao@gmail.com>
2014-09-23 18:37:48 +0800

    Fixed bugs

--- tests/test.py
+++ tests/test.py
@@ -15,4 +15,4 @@ class TestDriver(testing.Driver):
     def __init__(self):
         self.scheme = 'qiniustorage'
         self.path = ''
-        self.config = testing.Config({'qiniu_bucket':'docker-registry-qiniu','qiniu_accesskey':'V-Ua6icAAmPxxPhrTWIGoYFPHgz-aIiH7GCZDuJr','qiniu_secretkey':'SAx_l3aNFa8rcx-yfi_DTd_sOq8Iz_H8n5Zn6tVW','qiniu_domain':'docker-registry-qiniu.qiniudn.com'})
+        self.config = testing.Config({'qiniu_bucket':'docker-registry-qiniu','qiniu_accesskey':'V-Ua6icAAmPxxPhrTWIGoYFPHgz-aIiH7GCZDuJr','qiniu_secretkey':'','qiniu_domain':'docker-registry-qiniu.qiniudn.com'})
